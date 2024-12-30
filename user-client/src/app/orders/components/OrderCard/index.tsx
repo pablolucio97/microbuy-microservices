0f74c0a83ec6c1bf1e815024b622dedcce2e2741
Pablo <pablolucio_@hotmail.com>
2024-12-30 06:37:59 -0300

    styles: improves Orders page styles

--- user-client/src/app/orders/components/OrderCard/index.tsx
+++ user-client/src/app/orders/components/OrderCard/index.tsx
@@ -8,14 +8,20 @@ interface OrderCardProps {
 
 export default function OrderCard({ id, total, wasProcessed }: OrderCardProps) {
   return (
-    <div className="w-full flex flex-col md:flex-row md:items-center md:justify-between p-4 rounded-md mb-3 mr-3 bg-textWhite">
-      <strong>Order {id}</strong>
+    <div className="w-full flex flex-col md:flex-row md:items-center md:justify-between p-4 rounded-md mb-3 mr-3 bg-gradient-to-r from-gray-700 to-gray-800">
+      <strong className="text-white text-xs lg:text-[14px]">Order {id}</strong>
       <div className="flex items-center justify-between">
-        <span>{formatBRL(total)}</span>
+        <span className="text-white text-xs lg:text-[14px]">
+          {formatBRL(total)}
+        </span>
         {wasProcessed ? (
-          <span className="text-green-600 font-bold ml-8">Finished</span>
+          <span className="text-xs lg:text-[14px] text-green-600 font-bold ml-8">
+            Finished
+          </span>
         ) : (
-          <span className="text-orange-400 font-bold ml-8">Processing...</span>
+          <span className="text-xs lg:text-[14px] text-orange-400 font-bold ml-8">
+            Processing...
+          </span>
         )}
       </div>
     </div>
--- user-client/src/app/orders/page.tsx
+++ user-client/src/app/orders/page.tsx
@@ -3,13 +3,15 @@ import OrderCard from "./components/OrderCard";
 
 export default function Orders() {
   return (
-    <div className="w-screen min-h-screen flex flex-col overflow-x-hidden">
+    <div className="w-screen min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-gray-800 to-gray-900">
       <main className="w-full flex md:pl-[4rem]">
         <div className="w-full px-8">
-          <h1 className="text-textHeading text-2xl ml-3 font-bold mt-4">
+          <h1 className="text-textHeading text-xl md:text-2xl ml-3 font-bold my-2 text-white">
             MicroBuy
           </h1>
-          <span className="text-textHeading text-md ml-3 ">Your orders</span>
+          <span className="text-textHeading  text-sm md:text-xl ml-3 text-white mt-2 mb-3">
+            Your orders
+          </span>
           <div className="w-full max-h-[90vh] p-3 overflow-y-auto overflow-x-hidden">
             {orders.map((order) => (
               <OrderCard
