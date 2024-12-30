cfe37e0b66aa8f4914f3393ea3805b48e49ade54
Pablo <pablolucio_@hotmail.com>
2024-12-30 11:27:56 -0300

    feat: implements coupons listage using React Query

--- user-client/src/app/layout.tsx
+++ user-client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ClientQueryProvider from "./ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientQueryProvider>{children}</ClientQueryProvider>
       </body>
     </html>
   );
--- user-client/src/app/orders/page.tsx
+++ user-client/src/app/orders/page.tsx
@@ -1,7 +1,28 @@
-import { orders } from "@/data/mock";
+"use client";
+import { CouponsRepository } from "@/repositories/couponsRepostory/CouponsRepository";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
 import OrderCard from "./components/OrderCard";
 
 export default function Orders() {
+  const couponsRepository = useMemo(() => {
+    return new CouponsRepository();
+  }, []);
+
+  const listCoupons = async () => {
+    try {
+      const coupons = await couponsRepository.listCoupons();
+      return coupons;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["coupons"],
+    queryFn: listCoupons,
+  });
+
   return (
     <div className="w-screen min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-gray-800 to-gray-900">
       <main className="w-full flex md:pl-[4rem]">
@@ -13,14 +34,25 @@ export default function Orders() {
             Your orders
           </span>
           <div className="w-full max-h-[90vh] p-3 overflow-y-auto overflow-x-hidden">
-            {orders.map((order) => (
-              <OrderCard
-                key={order.id}
-                id={order.id}
-                total={order.total}
-                wasProcessed={order.was_processed}
-              />
-            ))}
+            {isLoading ? (
+              <span className="text-textHeading  text-sm md:text-xl ml-3 text-white mt-2 mb-3">
+                Loading...
+              </span>
+            ) : error ? (
+              <span className="text-textHeading  text-sm md:text-xl ml-3 text-white mt-2 mb-3">
+                Something went wrong. Please, try again later.
+              </span>
+            ) : (
+              data &&
+              data.map((order) => (
+                <OrderCard
+                  key={order.id}
+                  id={order.id}
+                  total={order.total}
+                  wasProcessed={order.processed}
+                />
+              ))
+            )}
           </div>
         </div>
       </main>
